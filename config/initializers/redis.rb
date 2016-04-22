aac59cd04d0f4bd1d7a0402a1b9358864c5d5c3b
Gerrit Riessen <gerrit.riessen@gmail.com>
2016-04-22 11:08:36 +0200

    yaml missing?!?
    On branch: master

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,7 @@
 require 'redis'
 require 'redis/connection/hiredis'
 require_relative '../../lib/redis_pool'
+require 'yaml'
 
 redis_config = begin
   config = open(File.join(File.dirname(__FILE__), "..", "redis.yml.erb")).read
